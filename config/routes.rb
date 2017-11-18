208660f63b1f323012108164918fd01fe96a3fac
Amine Aboufariss <farissamine@yahoo.com>
2017-11-18 09:34:10 +0000

    first

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  
+  
+  resources :line_items
+  resources :carts
+  get 'store/index'
+
+  resources :products
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'store#index'
+
+end
